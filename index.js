17f341b91da587516a1da07449c48635411d19c5
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-27 02:55:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object['key']
-  return object;
+  var newObject
+  delete newObject['key']
+  return newObject;
 }
