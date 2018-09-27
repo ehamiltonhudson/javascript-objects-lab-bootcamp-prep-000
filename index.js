6896840e3fbe2e3091f77947f1b43a1db3f12f66
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-27 02:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  prop: 1, prop: 2[key] = value
-  return obj
 }
