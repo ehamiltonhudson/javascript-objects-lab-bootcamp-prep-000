8aa9820c848756d7147387d014e40f8fc27250d2
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-27 02:20:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, recipes, { [key]: value} )
+  return Object.assign({obj = prop: 1, { [key]: value} )
 }
